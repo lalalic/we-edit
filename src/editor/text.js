c55d4138caf1ae73cd46551f05878c6425ac1e10
licheng <lalalic@139.com>
2016-07-06 22:47:52 +0800

    cursor

--- src/editor/text.js
+++ src/editor/text.js
@@ -9,9 +9,11 @@ export default class extends editable(Text){
 
     onClick(event, text){
         const {offsetX}=event.nativeEvent
-        let composer=new this.constructor.WordWrapper(text.children)
+        let style=this.getFontStyle()
+        let composer=new this.constructor.WordWrapper(this.state.content, style)
         let loc=composer.next({width:offsetX})||{end:0}
         let index=text.end-text.children.length+loc.end
-        this.setState({cursor:index, content:"Raymond changed it"}, a=>this.reCompose())
+        this.context.cursor.setState({x:event.pageX, y: event.pageY, height: loc.height, target:this, loc: index})
+        //this.setState({cursor:index, content:"Raymond changed it"}, a=>this.reCompose())
     }
 }
