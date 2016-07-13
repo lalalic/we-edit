5f48dd47a21c0d19a2f15f5dc1418510751d84a1
licheng <lalalic@139.com>
2016-07-14 06:33:09 +0800

    global cursor shape

--- src/editor/cursor.js
+++ src/editor/cursor.js
@@ -3,48 +3,51 @@ import ReactDOM from "react-dom"
 
 export default class Cursor extends Component{
 	static displayName="cursor"
-	state={target:null, textpiece: null}
-	
+	state={target:null, node: null, at: 0, width:0, height:0, style:{}}
+
     render(){
-		return null
+		const {width, height, style}=this.state
+		return (
+			<Shape ref="shape" width={width} height={height} style={style}/>
+		)
     }
-	
-	shouldComponentUpdate(nextProps, nextState){
-		const {textpiece}=this.state
-		const {textpiece:nextTextpiece}=nextState
-		if(textpiece!=nextTextpiece && textpiece)
-			this.state.target.blur(textpiece)
-		
-		return false
+
+	componentDidUpdate(prevProps, prevState){
+		const {target, node, at}=this.state
+		if(target && node){
+			node.appendChild(ReactDOM.findDOMNode(this.refs.shape))
+		}
 	}
-	
+
 	replaceFocusedContent(content){
-		this.state.target.insert(content)
+		const {target, at}=this.state
+		this.setState({node:null, at:at+content.length})
+		target.splice(at,0,content)
 	}
 }
 
 export class Shape extends Component{
 	render(){
 		const {width, height, style}=this.props
-		return <line 
-					x1={width} 
-					y1={0} 
-					x2={width} 
-					y2={-height} 
-					strokeWidth={1} 
+		return <line
+					x1={width}
+					y1={0}
+					x2={width}
+					y2={-height}
+					strokeWidth={1}
 					stroke={style.color||"black"}
 					/>
 	}
-	
+
 	componentDidMount(){
 		let node=ReactDOM.findDOMNode(this)
-		let x=-1000, show=false
+		let x=-1000, next=node.getAttribute('y2')
 		this.timer=setInterval(a=>{
-			node.setAttribute('transform',`translate(${show ? 0 : x},0)`)
-			show=!show
+			node.setAttribute('y1',next)
+			next=next ? 0: node.getAttribute('y2')
 		}, 700)
 	}
-	
+
 	componentWillUnmount(){
 		this.timer && clearInterval(this.timer)
 	}
--- src/editor/text.js
+++ src/editor/text.js
@@ -7,91 +7,70 @@ import {Shape as CursorShape} from "./cursor"
 
 let Super=editable(Text)
 export default class extends Super{
-	_refs=new Map()
-	
-    createComposed2Parent(props){
+	createComposed2Parent(props){
 		let composed=super.createComposed2Parent(...arguments)
 		let {width, height, children:text}=composed.props
 		text=React.cloneElement(text,{onClick:e=>this.onClick(e,props)})
-		
-        return (
-			<CursorableText 
-				ref={a=>this._refs.set(props,a)}
-				width={width} height={height} 
-				createCursor={a=>this.createCursorElement(props)}>
-				{text}
-			</CursorableText>
-		)
-    }
-	
-	createCursorElement(props){
-		const {end, children: textpiece}=props
-		const {cursorAt, content}=this.state
-        let style=this.getStyle()
 
-		if(cursorAt==undefined)
-			return null
-		
-		if(end-textpiece.length<cursorAt && cursorAt<=end){
-			let locText=content.substring(end-textpiece.length, cursorAt)
-			let composer=new this.constructor.WordWrapper(locText, style)
-			let size=composer.next({width:Number.MAX_SAFE_INTEGER})
-			size.height=composer.height
-			return <CursorShape {...size} style={style}/>
+		let cursor=this.context.cursor()||{state:{}}
+		let {target,at: cursorAt}=cursor.state
+		if(target!=this){
+			return (
+				<Group height={height} width={width}>
+				{text}
+				</Group>
+			)
 		}
-		
-		return null
-	}
+
+		let ps={width,height}
+		const {end, children: textpiece}=props
+
+		if(end-textpiece.length<cursorAt && cursorAt<=end){
+			ps.ref=a=>{
+				let node=ReactDOM.findDOMNode(a)
+				let text=textpiece.substr(0,cursorAt-(end-textpiece.length))
+				let composer=new this.constructor.WordWrapper(text, style)
+				let {contentWidth, fontFamily}=composer.next({width})||{end:0}
+				let style=this.getStyle()
+				cursor.setState({
+					target:this,
+					at:cursorAt,
+					node,
+					width:contentWidth,
+					height:composer.height, style})
+			}
+	        return (
+				<Group {...ps}>
+					{text}
+				</Group>
+			)
+		}else {
+			return composed
+		}
+    }
 
     onClick(event, text){
 		const {nativeEvent:{offsetX, offsetY}, target}=event
         let style=this.getStyle()
         let composer=new this.constructor.WordWrapper(text.children, style)
-        let loc=composer.next({width:offsetX})||{end:0}
-        let index=text.end-text.children.length+loc.end
-		this.setState({cursorAt:index}, a=>{
-			let cursor=this.context.cursor()
-			cursor.setState({target:this, textpiece:text}, a=>{
-				let ref=this._refs.get(text)
-				if(ref)
-					ref.setState({composedAt:new Date().toString()})
-			})
-		})
+        let {contentWidth,end}=composer.next({width:offsetX})||{end:0,contentWidth:0}
+        let index=text.end-text.children.length+end
+		let cursor=this.context.cursor()
+		cursor.setState({
+			target:this,
+			at: index,
+			node:event.target.parentNode,
+			width:Math.ceil(contentWidth),
+			height:composer.height,
+			style })
     }
 
-	insert(str){
-		const {content, cursorAt}=this.state
-		this.setState({
-			content:content.splice(cursorAt,0,str), 
-			cursorAt:cursorAt+str.length},e=>{
-				this.reCompose()
-			})
-	}
-
-	blur(text){
-		console.log(`blured ${JSON.stringify(text)}`)
-		this.setState({cursorAt:undefined}, a=>{
-			let ref=this._refs.get(text)
-			if(ref)
-				ref.setState({composedAt: new Date().toString()})
-		})
+	splice(start, length, str){
+		const {content}=this.state
+		this.setState({content:content.splice(start,length,str)},e=>this.reCompose())
 	}
 
 	static contextTypes=Object.assign({
 		cursor: PropTypes.func
 	},Super.contextTypes)
-}
-
-class CursorableText extends Group{
-	render(){
-		const {width, height, children, createCursor}=this.props
-		
-		let cursor=createCursor()
-		return (
-			<Group width={width} height={height}>
-				{createCursor()}
-				{children}
-			</Group>
-		)
-	}
 }
