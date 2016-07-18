febdfc1f55c4b0ab4600a7ee5f54d14675ca5ddd
lalalic <lalalic@gmail.com>
2016-07-18 09:10:03 +0800

    rollback to docxAsElement

--- src/input/docx/index.js
+++ src/input/docx/index.js
@@ -1,7 +1,6 @@
 import docx4js from "docx4js"
 import Base from "../base"
-import React, {Component} from "react"
-import {PassThrough} from "stream"
+
 
 export default class Docx extends Base{
 	static support(file){
@@ -19,54 +18,6 @@ export default class Docx extends Base{
 				else
 					return doc=new Document(wordModel)
 			}))
-		})
-	}
-
-	load2(data){
-		return docx4js.load(data).then(docx=>{
-
-			function createComponent(type){
-				return class Content extends Component{
-					static displayName=type
-					render(){
-						return <div>{this.props.children}</div>
-					}
-				}
-			}
-
-			return new Promise((resolve, reject)=>{
-				let root={name:"docx",children:[]}
-				let current=root
-				let data=docx.parts[docx.rels.officeDocument]
-				let stream=new PassThrough()
-				stream.end(new Buffer(data.asUint8Array()))
-				stream.pipe(require("sax").createStream(true,{xmlns:false})
-					.on("error", e=>{
-						console.error(e)
-					}).on("opentag", node=>{
-						current.children.push(node)
-						node.parent=current
-						current=node
-						node.children=[]
-					}).on("closetag",tag=>{
-						const {attributes, parent, children, local,name}=current
-						let index=parent.children.indexOf(current)
-						attributes.key=index
-						let element=React.createElement(createComponent(name), attributes, children)
-						parent.children.splice(index,1,element)
-						current=parent
-					}).on("end", a=>{
-						resolve({
-							createReactElement(){
-								return React.createElement(createComponent("docx"), null, root.children)
-							}
-						})
-					}).on("text", text=>{
-						if(current!==root)
-							current.children.push(text)
-					})
-				)
-			})
 		})
 	}
 }
