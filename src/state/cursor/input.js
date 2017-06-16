d29c5591c2f52cc6d205bca6f0970168fefa56e9
Li <lalalic@gmail.com>
2017-06-16 15:46:59 +0800

    fix cursor display

--- src/state/cursor/input.js
+++ src/state/cursor/input.js
@@ -4,19 +4,23 @@ import Waypoint from "react-waypoint"
 import Listener from "./listener"
 
 export default class Input extends Component{
-	state={top:0,left:0,height:0,inView:true}
+	state={top:0,left:0,height:0}
+	isInView(left,top){
+		let {scrollX,scrollY,screen:{height,width}}=window
+		return scrollY<top<scrollY+height && scrollX<left<scrollX+width
+	}
 	render(){
-		const {top,left,height,color,fonts,size,up,down,inView}=this.state
+		const {top,left,height,color,fonts,size,up,down}=this.state
 		let style={height,margin:0,padding:0,border:0,left:0,top:0,position:"absolute",outline:"none"}
-		if(!inView){
+		if(!this.isInView(left,top)){
 			style.position="fixed"
 			style.height=1
 		}
 
 		return (
 			<Waypoint fireOnRapidScroll={false}
-				onEnter={e=>this.setState({inView:true})}
-				onLeave={e=>this.setState({inView:false})}
+				onEnter={e=>this.forceUpdate()}
+				onLeave={e=>this.forceUpdate()}
 				>
 				<div unselectable="on"
 					style={{left,top:top,position:"absolute",height:0,width:0}}>
