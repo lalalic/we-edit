71ac8b7b7d1818679257e69848314593ea1cc3f2
Li <lalalic@gmail.com>
2018-02-14 14:18:51 +0800

    init status

--- src/ui/status/index.js
+++ src/ui/status/index.js
@@ -1,9 +1,10 @@
 import React from "react"
-import {Toolbar, ToolbarGroup, FlatButton} from "material-ui"
+import {Toolbar, ToolbarGroup, FlatButton, Slider} from "material-ui"
 
 const ButtonStyle={
 	background:"transparent",
-	color:"white"
+	color:"white",
+	fontSize: 8,
 }
 
 const RootStyle={
@@ -20,6 +21,7 @@ const Status=({})=>(
 		<Toolbar style={ButtonStyle}>
 			<Page/>
 			<Words/>
+			<Scale/>
 		</Toolbar>
 	</div>
 )
@@ -27,5 +29,16 @@ const Status=({})=>(
 const Page=({current=1,total=20})=>(<FlatButton style={ButtonStyle}>PAGE {current} OF {total}</FlatButton>)
 
 const Words=({total=120})=>(<FlatButton style={ButtonStyle}>{total} WORDS</FlatButton>)
+
+const Scale=()=>(
+	<div style={{width:400,display:"inline-block"}}>
+		<div style={{width:300,display:"inline-block"}}>
+			<Slider 
+				sliderStyle={{marginTop:6}} 
+				step={5} 
+				value={100} min={20} max={200}/>
+		</div>
+	</div>
+)
 
 export default Status
