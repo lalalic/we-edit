94b0ea4230a5348cab90101d5dadcd68ab2a17f5
Li <lalalic@gmail.com>
2018-03-22 18:49:35 +0800

    refactor

--- webpack.npm.js
+++ webpack.npm.js
@@ -1,22 +1,28 @@
 const path = require('path')
 const webpack = require('webpack')
+const { execSync } = require('child_process');
+
 process.env.NODE_ENV='production'
 
-console.dir(process.env)
 
 module.exports=(base, packages)=>{
+	packages
+		.forEach(p=>{
+			execSync(`yarn compile packages/${p}/src -d packages/${p}/lib`)
+			console.log('compiled '+p)
+		})
 	return packages.map(p=>{
-		return {
-			...base,
-			entry:`./packages/${p}/src/index.js`,
-			output:{
-				filename:`index.${process.env.NODE_ENV}.js`,
-				path: path.resolve(`${__dirname}/packages/${p}`, 'dist')
-			},
-			externals:{
-				react:"react",
-				"we-edit":"we-edit"
+			return {
+				...base,
+				entry:`./packages/${p}/src/index.js`,
+				output:{
+					filename:`index.${process.env.NODE_ENV}.js`,
+					path: path.resolve(`${__dirname}/packages/${p}`, 'dist')
+				},
+				externals:{
+					react:"react",
+					"we-edit":"we-edit"
+				}
 			}
-		}
-	})
+		})
 }
