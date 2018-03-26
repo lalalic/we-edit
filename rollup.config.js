af3fef0b261d48f3eb420826470a18deea38b14f
licheng <lalalic@139.com>
2018-03-26 23:24:55 +0800

    rollup can

--- rollup.config.js
+++ rollup.config.js
@@ -1,39 +1,59 @@
 const babel = require("rollup-plugin-babel");
 const commonjs = require("rollup-plugin-commonjs");
 const resolve = require("rollup-plugin-node-resolve");
+const minify=require("rollup-plugin-uglify")
 
-export default {
-  input: "packages/we-edit/src/index.js",
-  output:{
-	file: "packages/we-edit/cjs/index.production.js",  
-	format: "cjs",
-  },
-  plugins: [
-    resolve(),
-    
-    babel({
-		babelrc:false,
-		presets: [
-			["env", {modules:false}],
-			"react",
-		],
-		plugins:[
-			"babel-plugin-external-helpers",
-			"babel-plugin-add-module-exports",
-			"babel-plugin-transform-object-rest-spread",
-			"babel-plugin-transform-class-properties",
-		]
-	}),
-	
-	commonjs({
-		include: 'node_modules/**',
-		namedExports:{
-			'node_modules/react/index.js': [
-				'Component', 'PureComponent', 
-				'Children', 'createElement'
-			],
-			'node_modules/prop-types/index.js':['default']
-		}
-	}),
-  ]
+export function config(project,mode="production"){
+    let base={
+      input: `packages/${project}/src/index.js`,
+      output:{
+    	file: `packages/${project}/cjs/index.${mode}.js`,
+    	format: "cjs",
+      },
+      plugins: [
+        resolve({
+            browser:true,
+        }),
+
+        babel({
+    		babelrc:false,
+    		presets: [
+    			["env", {modules:false}],
+    			"react",
+    		],
+            exclude: "node_modules/**",
+    		plugins:[
+    			"babel-plugin-external-helpers",
+    			//"babel-plugin-add-module-exports",
+    			"babel-plugin-transform-object-rest-spread",
+    			"babel-plugin-transform-class-properties",
+    		]
+    	}),
+
+    	commonjs({
+    		namedExports:{
+    			'node_modules/react/index.js': [
+    				'Component', 'PureComponent',
+    				'Children', 'createElement',
+                    'Fragment', 'createFactory'
+    			],
+                "node_modules/immutable/dist/immutable.js":[
+                    "List","Map","Collection"
+                ],
+    		}
+    	}),
+
+
+      ]
+    }
+
+    if(mode=="production"){
+        base.plugins.push(minify())
+    }else{
+        base.output.sourcemap=true
+    }
+    console.dir(base)
+    return base
 }
+
+export default config("we-edit")
