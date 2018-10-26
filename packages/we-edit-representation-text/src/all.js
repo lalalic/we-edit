dc87402b8d0a93c5f9da3dea04cfec3e0026fb13
lalalic <lalalic@139.com>
2018-10-26 17:16:12 +0800

    clean code

--- packages/we-edit-representation-text/src/all.js
+++ packages/we-edit-representation-text/src/all.js
@@ -5,22 +5,8 @@ import Document from "./document"
 import Paragraph from "./paragraph"
 import Text from  "./text"
 
-function textify(Model){
-	return class extends Model{
-		render(){
-			return <Fragment>{this.props.children}</Fragment>
-		}
-	}
-}
-
-export default Object.keys(models).reduce((converted,k)=>{
-	if(converted[k]===undefined){
-		converted[k]=textify(models[k])
-	}
-	return converted
-},{
+export default Object.assign({...models},{
 	Document,
 	Paragraph,
 	Text,
-	
 })
