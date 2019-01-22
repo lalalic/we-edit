5a8b73a5cf60b6c2d209b5ba8dfd0de07550af78
lalalic <lalalic@139.com>
2019-01-22 16:49:13 +0800

    cache single page table

--- packages/we-edit-representation-pagination/src/edit/table.js
+++ packages/we-edit-representation-pagination/src/edit/table.js
@@ -7,7 +7,44 @@ import Base from "../table"
 
 
 export default Cacheable(class extends editable(Base){
+	constructor(){
+		super(...arguments)
+		this.computed.spaces=[]
+	}
+
+	nextAvailableSpace(){
+		const space=super.nextAvailableSpace(...arguments)
+		const frame=space.frame
+		if(this.computed.spaces.indexOf(frame)==-1){
+			this.computed.spaces.push(frame)
+		}
+		return space
+	}
+
+	clearComposed(){
+		this.computed.spaces=[]
+		super.clearComposed(...arguments)
+	}
+
 	appendLastComposed(){
-		return false
+		if(this.computed.spaces.length>1){
+			return false
+		}
+		const space=super.nextAvailableSpace()
+		const height=this.computed.lastComposed.reduce((h,{props:{height}})=>h+=height,0)
+		if(space.height<height){
+			return false
+		}
+		try{
+			this.computed.lastComposed.forEach(a=>this.context.parent.appendComposed(a))
+			this.computed.spaces=[space.frame]
+		}catch(e){
+			console.warn(e)
+			return false
+		}
+	}
+
+	createComposed2Parent(){
+		return super.createComposed2Parent(...arguments)
 	}
 })
