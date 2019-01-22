cab060f5799d50b6bae9f7f2c5e0afd970f456cf
lalalic <lalalic@139.com>
2019-01-22 22:11:31 +0800

    move content

--- packages/we-edit/src/input/reducer/entity.js
+++ packages/we-edit/src/input/reducer/entity.js
@@ -70,7 +70,16 @@ export class entity extends Content{
 		return this
 	}
 
-	move({id,at}){
+	move(dest){
+		const {start:{id}}=this.selection
+		const content=this.$('#'+id)
+		const paragraph=content.closest("paragraph")
+		content.remove()
+		const [to]=this.splitAtUpto(dest)
+		const newContent=this.file.construct(id,paragraph.attr("id"))
+		newContent.insertAfter(to)
+		this.renderChanged(paragraph.attr("id"))
+		this.renderChanged(to.closest("paragraph").attr("id"))
 		return this
 	}
 }
