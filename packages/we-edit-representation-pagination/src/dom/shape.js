0e059b473f96aa850b234695e3425913f9a73714
lalalic <lalalic@139.com>
2019-04-12 17:42:27 +0800

    refactor shape

--- packages/we-edit-representation-pagination/src/dom/shape.js
+++ packages/we-edit-representation-pagination/src/dom/shape.js
@@ -1,4 +1,4 @@
-import React from "react"
+import React,{Component} from "react"
 import PropTypes from "prop-types"
 
 
@@ -9,28 +9,81 @@ import {dom} from "we-edit"
 const {Shape:Base}=dom
 const Super=HasParentAndChild(Base)
 
-class Area{
-	constructor(props){
-		this.props=props
+class custom extends Component{
+	availableSpace(){
+		const {width,height,margin}=this.props
+		return {
+			width: width-margin.left-margin.right,
+			height: height-margin.top-margin.bottom
+		}
 	}
-	availableSpace(){}
-	createComposedShape(){}
+
+	createComposedShape(content){
+		const {
+				width,height,margin,solidFill="transparent",blipFill:{url}={},
+				outline={strokeWidth:0,stroke:"black"},
+				fill={fill:solidFill}
+			}=this.props
+
+		return [
+			<path key="shape"
+				d={this.getPath()}
+				style={{...outline, ...fill}} />,
+			url&&(<image key="image"
+				{...{width,height,xlinkHref: url, preserveAspectRatio:"none"}} />),
+			<Group key="content"
+				x={margin.left}
+				y={margin.top}
+				children={content}/>
+		]
+	}
+
+	getPath(){
+		return this.props.geometry
+	}
+}
+
+class rect extends custom{
+	getPath(){
+		const {width:w,height:h}=this.props
+		return `M0 0h${w}v${h}h${-w}z`
+	}
+}
+
+class ellipse extends custom{
+	createComposedShape(content){
+		const {
+				width,height,margin,solidFill="none",
+				outline={strokeWidth:1,stroke:"black"},
+				fill={fill:solidFill}
+			}=this.props
+		const composed=super.createComposedShape(...argument)
+		composed.splice(0,1,
+			<ellipse key="shape"
+			cx={width/2} cy={height/2}
+			rx={width/2} ry={height/2}
+			style={{...outline, ...fill}} />)
+		return composed
+	}
+}
+
+class circle extends ellipse{
+
 }
 
 export default class Shape extends Super{
 	constructor(){
 		super(...arguments)
-		const {shape="custom"}=this.props
-		debugger
-		if(this.constructor[shape]){
-			this.shape=new this.constructor[shape](...arguments)
+		const {geometry="rect"}=this.props
+		if(this.constructor[geometry]){
+			this.geometry=new this.constructor[geometry](...arguments)
 		}else{
-			
+
 		}
 	}
 
 	nextAvailableSpace(){
-		return this.shape.availableSpace()
+		return this.geometry.availableSpace()
 	}
 
 	appendComposed(content){
@@ -44,114 +97,68 @@ export default class Shape extends Super{
 		super.onAllChildrenComposed()
 	}
 
+	children(){
+		const children=React.Children.toArray(this.props.children)
+		if(children.length){
+			return React.cloneElement(children[0],this.nextAvailableSpace())
+		}
+		return null
+	}
+
 	createComposed2Parent(){
-		let {width,height,margin,position}=this.props
+		const {width,height,margin,x,y}=this.props
 		this.context.parent.nextAvailableSpace({width,height})
-		let y=0
-		let content=this.computed.composed.map((a,i)=>{
-			if(y+a.props.height>height)
+		let Y=0
+		const content=this.computed.composed.map((a,i)=>{
+			if(Y+a.props.height>height)
 				return null
-			let r=<Group y={y} key={i} children={a}/>
-			y+=a.props.height
+			let r=<Group y={Y} key={i} children={a}/>
+			Y+=a.props.height
 			return r
 		})
+
 		return (
-			<Group {...{width,height}}>
-				<Group x={0} y={-height}>
-					<Group {...position}>
-						{this.shape.createComposedShape(content)}
-					</Group>
+			<Group {...{width,height,x,y}}>
+				{this.transform(this.geometry.createComposedShape(content))}
+			</Group>
+		)
+	}
+
+	transform(element){
+		const {rotate,scale}=this.props
+		if(scale){
+
+		}
+
+		if(rotate){
+			element=this.rotate(element)
+		}
+
+		return element
+	}
+
+	rotate(element){
+		const {rotate}=this.props
+		const {width,height}=element.props
+		const radians=rotate*Math.PI/180
+		const rotatedHeight=height * Math.abs(Math.cos(radians)) + width * Math.abs(Math.sin(radians))
+		return (
+			<Group {...{
+				width:width * Math.abs(Math.cos(radians)) + height * Math.abs(Math.sin(radians)),
+				height:rotatedHeight,
+			}}>
+				<Group {...{x:height*Math.abs(Math.sin(radians)),rotate}}>
+					{element}
 				</Group>
 			</Group>
 		)
 	}
 
-	static rect=class extends Area{
-		availableSpace(){
-			const {width,height,margin}=this.props
-			return {
-				width: width-margin.left-margin.right,
-				height: height-margin.top-margin.bottom
-			}
-		}
+	static custom=custom
 
-		createComposedShape(content){
-			let {
-					width,height,margin,
-					outline={strokeWidth:1,stroke:"black"},
-					fill={fill:"none"}
-				}=this.props
+	static rect=rect
 
-			return [
-				<rect key="shape" x="0" y="0"
-							width={width} height={height}
-							style={{...outline, ...fill}}/>,
+	static ellipse=ellipse
 
-				<Group key="content" x={margin.left} y={margin.top} children={content}/>
-			]
-		}
-	}
-
-	static ellipse=class extends Area{
-		availableSpace(){
-			const {width,height,margin}=this.props
-			return {
-				width: Math.sqrt(2)*width/2-margin.left-margin.right,
-				height: Math.sqrt(2)*height/2-margin.top-margin.bottom
-			}
-		}
-
-		createComposedShape(content){
-			let {
-					width,height,margin,
-					outline={strokeWidth:1,stroke:"black"},
-					fill={fill:"none"}
-				}=this.props
-
-			let size=this.availableSpace()
-			return [
-				<ellipse key="shape"
-					cx={width/2} cy={height/2}
-					rx={width/2} ry={height/2}
-					style={{...outline, ...fill}} />,
-
-				<Group key="content"
-					x={width/2-size.width/2}
-					y={height/2-size.height/2}
-					children={content}/>
-			]
-		}
-	}
-
-	static circle=Shape.ellipse
-
-	static custom=class extends Area{
-		availableSpace(){
-			const {width,height,margin}=this.props
-			return {
-				width: width-margin.left-margin.right,
-				height: height-margin.top-margin.bottom
-			}
-		}
-
-		createComposedShape(content){
-			let {
-					width,height,margin,
-					path,
-					outline={strokeWidth:1,stroke:"black"},
-					fill={fill:"none"}
-				}=this.props
-
-			return [
-				<path key="shape"
-					d={path}
-					style={{...outline, ...fill}} />,
-
-				<Group key="content"
-					x={margin.left}
-					y={margin.top}
-					children={content}/>
-			]
-		}
-	}
+	static circle=circle
 }
--- packages/we-edit/src/dom/shape.js
+++ packages/we-edit/src/dom/shape.js
@@ -5,11 +5,11 @@ import Component from "./component"
 
 export default class Shape extends Component{
 	static displayName="shape"
-	
+
 	static propTypes={
 		position:PropTypes.shape({
 			x:PropTypes.number,
-			y:PropTypes.number 
+			y:PropTypes.number
 		}),
 		width:PropTypes.number.isRequired,
 		height:PropTypes.number.isRequired,
@@ -18,6 +18,23 @@ export default class Shape extends Component{
 			right:PropTypes.number,
 			top:PropTypes.number,
 			bottom:PropTypes.number
-		})
+		}),
+		geometry:PropTypes.string,//
+		outline:PropTypes.any,
+		solidFill: PropTypes.string,
+		blipFill: PropTypes.shape({
+			url: PropTypes.string,
+		}),
+		rotate:PropTypes.number,
+		scale:PropTypes.number,
+	}
+
+	static defaultProps={
+		margin:{
+			left:0,
+			right:0,
+			top:0,
+			bottom:0,
+		}
 	}
 }
