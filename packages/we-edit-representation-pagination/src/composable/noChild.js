e9f1af3b905ec7c3f9a8fcb558d6c8150f28aa4b
lalalic <lalalic@139.com>
2019-11-18 18:20:55 +0800

    recover nochild.getMyBreakOpportunities

--- packages/we-edit-representation-pagination/src/composable/noChild.js
+++ packages/we-edit-representation-pagination/src/composable/noChild.js
@@ -4,12 +4,18 @@ import HasParentAndChild from "./hasParentAndChild"
 export default A=>{
     const Super=HasParentAndChild(A)
     return class __$1 extends Super{
-		constructor(){
+		static contextTypes={
+            ...Super.contextTypes,
+            getMyBreakOpportunities: PropTypes.func
+        }
+
+        constructor(){
             super(...arguments)
             this.splittable=false
         }
 
         render() {
+            this.context.getMyBreakOpportunities(null)
             this.appendComposed()
             this.onAllChildrenComposed()
             return null
