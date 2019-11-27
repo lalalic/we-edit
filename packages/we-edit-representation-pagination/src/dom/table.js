123953babeb65e2d7e7dac1df62ef80653f71656
lalalic <lalalic@139.com>
2019-11-27 15:21:32 +0800

    table has no impact on required space

--- packages/we-edit-representation-pagination/src/dom/table.js
+++ packages/we-edit-representation-pagination/src/dom/table.js
@@ -13,15 +13,6 @@ const Super=HasParentAndChild(dom.Table)
  * space is defined by cell->row->table->parent space, so it has to require space up
  */
 export default class Table extends Super{
-	/** table defines row width, but block size is ensured by parent space */
-	//***simplified: not consider wrappees
-	//if table height is specified, it should be considered to require space
-	//table width has nothing to do with cell layout since row has cols to constraint cell layout space
-	nextAvailableSpace({height:requiredBlockSize=0}={}){
-		const {width,height=0}=this.props
-		return this.context.parent.nextAvailableSpace({height:Math.max(height,requiredBlockSize)})
-	}
-
 	/**row call it to append a block of row*/
 	createComposed2Parent(row){
 		const {width,indent}=this.props
