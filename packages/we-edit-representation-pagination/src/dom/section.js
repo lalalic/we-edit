3e47cf7383b4f3dc5fda95d120aa7d5f15716c55
lalalic <lalalic@139.com>
2019-12-18 18:16:54 +0800

    remove fissionable from section

--- packages/we-edit-representation-pagination/src/dom/section.js
+++ packages/we-edit-representation-pagination/src/dom/section.js
@@ -1,9 +1,10 @@
 import React,{Children} from "react"
-import {HasParentAndChild,Fissionable, editable} from "../composable"
+import PropTypes from "prop-types"
+import {HasParentAndChild, editable} from "../composable"
 import {dom} from "we-edit"
-import memoize from "memoize-one"
+import Frame from "./frame"
 
-const Super=Fissionable(HasParentAndChild(dom.Section))
+const Super=HasParentAndChild(dom.Section)
 class Section extends Super{
 	static defaultProps={
 		...Super.defaultProps,
@@ -17,23 +18,138 @@ class Section extends Super{
 		}
 	}
 
-    getDocument=memoize(()=>{
-		var current=this.context.parent
-		while(current){
-			if(current.getComposeType()=="document")
-				return current
-            current=current.context.parent
-		}
-		return current
-	})
-
-	get totals(){
-		return this.getDocument().computed.composed.length
+	static childContextTypes={
+		...Super.childContextTypes,
+        prevLayout: PropTypes.func,
 	}
 
-    create(){
-        return super.create({I:this.totals})
+    /**
+     * why use static function to inherit??? because Frame is in instance's context
+     * @param {*} Frame 
+     */
+	static fissureLike(Frame){
+		return Frame
     }
+    
+	constructor(){
+		super(...arguments)
+		this.computed.named={}
+		const Fission=class extends this.constructor.fissureLike(Frame){
+            createComposed2Parent(){
+                const {props:{i,margin}}=this
+                return React.cloneElement(super.createComposed2Parent(...arguments),{margin,i,key:i})
+            }
+		}
+		Object.defineProperties(this,{
+			Fission:{
+				get:()=>Fission
+			},
+			isFissionable:{
+				get:()=>true,
+			},
+		})
+    }
+	get current(){
+        if(this.computed.composed.length==0){
+            const a=this.createLayout()
+            this.computed.composed.push(a)
+            this.context.parent.appendComposed(this.createComposed2Parent(a))
+        }
+		return this.computed.composed[this.computed.composed.length-1]
+	}    
+
+    getChildContext(){
+        const self=this
+        return {
+            ...super.getChildContext(),
+            prevLayout(ref){
+                const {composed}=self.computed
+                return composed[composed.indexOf(ref)-1]
+            }
+        }
+    }
+
+	named(name){
+		return this.computed.named[name]
+	}
+
+
+	/**page index, or undefined */
+	get topIndex(){
+		var current=this.context.parent
+		while(current){
+			if(current.isFrame || current.isFissionable)
+				return 
+			if(!current.context || !current.context.parent)
+				return current.computed.composed.length
+		}
+	}
+
+    /**
+     * ** create is pure, so you have to append to your composed and parent manually every time create called***
+     * create a block layout engine with a ensured space {left,right,blockOffset,height,wrappees}
+     * when current space is full, it would be called
+     * @param {*} props
+     * @param {*} context 
+     * @param {*} requiredSpace 
+     */
+    createLayout(props={},context={},requiredSpace){
+		const allProps={...props,
+			id:this.props.id, 
+			i:this.computed.composed.length, 
+			named:this.named.bind(this),
+		}
+		const I=this.topIndex
+		if(typeof(I)=="number")
+			allProps.I=I
+		return this.props.createLayout.bind(this)(
+			allProps,
+            {...context,parent:this,getComposer:id=>this.context.getComposer(id)}
+        )
+    }
+
+    createComposed2Parent(a){
+        return a
+    }
+
+    /**
+     * it proxy the call to current layout
+     * if current layout has no required space, a new Layout will be created
+     * @param {*} required 
+     */
+    nextAvailableSpace(required){
+        const space=this.current.nextAvailableSpace(...arguments)
+        if(!space){
+            const a=this.createLayout(undefined,{frame:space.frame},required)
+            this.computed.composed.push(a)
+            this.context.parent.appendComposed(this.createComposed2Parent(a))
+            return this.nextAvailableSpace(...arguments)
+        }
+        return space
+    }
+
+    /**
+     * named is supported to be kept
+     * @param {*} composedChildenContent 
+     * @returns
+     * number: to rollback last number of lines
+     */
+    appendComposed({props:{named,height}}){
+        if(named){
+            this.computed.named[named]=arguments[0]
+            return
+        }else{
+            const appended=this.current.appendComposed(...arguments)
+            if(appended===false){
+                const a=this.createLayout(undefined, undefined,{height})
+                this.computed.composed.push(a)
+                this.context.parent.appendComposed(this.createComposed2Parent(a))
+                return 1//recompose current line in case different availableSpace
+            }else if(Number.isInteger(appended)){
+                return appended
+            }
+        }
+	}
 }
 
 export default class EditableSection extends editable(Section,{stoppable:true}){
