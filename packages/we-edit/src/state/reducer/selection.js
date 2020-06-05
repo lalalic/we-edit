372ddb3e892d1510e08da2c508112647e2a53ce0
lalalic <lalalic@139.com>
2020-06-05 18:24:54 +0800

    remove activated

--- packages/we-edit/src/state/reducer/selection.js
+++ packages/we-edit/src/state/reducer/selection.js
@@ -3,7 +3,7 @@ export default (state, {type,payload})=>{
 	case `we-edit/selection/SELECTED`:
 		return {...state, cursorAt:"end",...payload}
 	case `we-edit/selection/CANVAS`:
-		return {...state, active:payload, actived:Date.now()}
+		return {...state, active:payload}
 	case "we-edit/selection/STARTAT":
 		return {...state, cursorAt:"start", start:payload}
 	default:
