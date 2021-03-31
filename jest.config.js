94a98ac129fd25577cc75323c195961539ccedfd
lalalic <lalalic@139.com>
2021-03-31 14:46:57 +0800

    fix debug/test configuration

--- jest.config.js
+++ jest.config.js
@@ -1,10 +1,10 @@
 module.exports={
 	modulePathIgnorePatterns:[require.resolve("./package.json"), "src/", "responsible/"],
+	
+	/**NOTE: don't change the map, otherwise debug/test doesn't work */
 	moduleNameMapper:{
 		"^we-edit(.*)/__tests__(.*)":`<rootDir>/packages/we-edit$1/__tests__$2`,
 		"^we-edit(.*)/(.*)":`<rootDir>/packages/we-edit$1/src/$2`,
 		"^we-edit(.*)":`<rootDir>/packages/we-edit$1/src`,
 	},
-	//watchPathIgnorePatterns:['<rootDic>/node_modules/'],
-	watchman:true,
 }
