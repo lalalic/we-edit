68d80708a6b28379474749f15425352b13406ed5
lalalic <lalalic@139.com>
2021-04-01 10:27:49 +0800

    clean container props

--- packages/we-edit-representation-pagination/src/dom/container.js
+++ packages/we-edit-representation-pagination/src/dom/container.js
@@ -11,21 +11,18 @@ export default class Container extends Stoppable(HasParentAndChild(dom.Container
     createComposed2Parent(element){
         const {createComposed2Parent}=this.props
         const {
-            width,height,minWidth,x,y,blockOffset,
+            width,height,minWidth,tabWidth,
+            x,y,blockOffset,
             wrap, anchor,
             descent,pagination,
             tokenizeOpportunity,
-            mergeOpportunity,tabWidth,
-            ...props}=element.props
+            }=element.props
         const myElement=React.isValidElement(element) ? React.createElement(Group,{
             width,height,minWidth,wrap, anchor,descent,pagination,x,y,blockOffset,tokenizeOpportunity,tabWidth,
-            children:React.cloneElement(element,{
-                x:undefined, y:undefined,wrap:undefined,anchor:undefined,blockOffset:undefined,
-                tokenizeOpportunity:undefined,
-                mergeOpportunity:undefined,
-                tabWidth:undefined,
-            })
+            children:React.cloneElement(element,clear(element.props))
         }) : element
         return createComposed2Parent ? createComposed2Parent(myElement,element) : myElement
     }
 }
+const Clear_Props="x,y,dy,wrap,anchor,blockOffset,tokenizeOpportunity,mergeOpportunity,tabWidth".split(",")
+const clear=props=>Clear_Props.reduce((props,k)=>(delete props[k], props),{...props})
